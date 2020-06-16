55df3c8fdf557f9aae212a8412197926df1b9395
Rodney D. Elliott <rodney.elliott@colorado.edu>
2020-06-15 19:18:38 -0600

    Made all constants inline and within their own namespace

--- src/constants.hpp
+++ src/constants.hpp
@@ -10,14 +10,17 @@
 
 #include <cmath>
 
-const double pi 	= M_PIl;            // pi [unitless]
-const double twopi 	= 2*pi;             // 2*pi [unitless]
-const double k_b 	= 1.3806504e-23;    // Boltzmann's Constant [J/K]
-const double c 		= 299792458.0;      // Speed of Light in Vacuum [m/s]
-const double G 		= 6.67428e-11;      // Gravitational Constant [m^3/kg/s^2]
-const double amu 	= 1.660538782e-27;  // Atomic Mass Unit [kg]
-const double m_e 	= 9.10938215e-31;   // Electron Mass [kg]
-const double q_e 	= 1.602176487e-19;  // Elementary Charge [C]
-const double jev 	= q_e;              // Joules/Electron Volt [unitless]
+namespace constants
+{
+	inline const double pi    = M_PIl;            // pi [unitless]
+	inline const double twopi = 2*pi;             // 2*pi [unitless]
+	inline const double k_b   = 1.3806504e-23;    // Boltzmann's Constant [J/K]
+	inline const double c     = 299792458.0;      // Speed of Light in Vacuum [m/s]
+	inline const double G     = 6.67428e-11;      // Gravitational Constant [m^3/kg/s^2]
+	inline const double amu   = 1.660538782e-27;  // Atomic Mass Unit [kg]
+	inline const double m_e   = 9.10938215e-31;   // Electron Mass [kg]
+	inline const double q_e   = 1.602176487e-19;  // Elementary Charge [C]
+	inline const double jev   = q_e;              // Joules/Electron Volt [unitless]
+}
 
 #endif /* CONSTANTS_HPP_ */
